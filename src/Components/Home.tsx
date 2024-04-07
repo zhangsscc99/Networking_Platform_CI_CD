46c2fc0849de9d94072a3b05ad51a3d31f899c98
Yuting Wang <yw4554@nyu.edu>
2024-04-07 14:37:49 -0500

    test variable

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,4 +1,5 @@
 import user_4_fill from '../Images/user_4_fill.png';
+import React, { useState, useEffect } from 'react';
 import humaaansGraph from '../Images/Humaaans Graphs.png';
 import '../Styles/Home.modules.css';
 
@@ -11,11 +12,35 @@ interface Props {
 }
 
 function Home({getStartedHandler, aboutUsHandler, contactHandler, FAQHandler, ProfileHandler}: Props) {
+    const [testVariable, setTestVariable] = useState(0);
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        // 确保URL与后端API端点匹配
+        fetch('http://localhost:8080/api/users/testVariable')
+            .then(response => response.json())
+            .then(data => setTestVariable(data));
+    }, []);
+
+    const showUsersHandler = () => {
+        fetch('http://localhost:8080/api/users')
+            .then(response => response.json())
+            .then(data => {
+                setUsers(data); // 假设返回的数据是用户对象的数组
+            });
+    };
+
+    
+    
     return (
         <div>
             <div id="website-title">Website.com</div>
             <h1> Welcome To Our Computer Networking World!</h1>
             <div id="intro">
+
+                {/* 在这里使用testVariable */}
+                <p>Test Variable Value: {testVariable}</p>
+        
                 This is the website that helps you understand everything of computer networking through games
             </div>
             <button id="get-started" onClick={getStartedHandler}>
